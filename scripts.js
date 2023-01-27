852688570aeea1ca00411858bc31daf3d9d2212c
OLIOLI2 <122591044+OLIOLI2@users.noreply.github.com>
2023-01-27 23:58:18 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -340,7 +340,7 @@ const onSubmitContractWrite = async (event) => {
     button.setAttribute('disabled', true);
 
     // Setup Interface + Encode Function
-    const SetGreeting = CONTRACT_ABI.find(i => i.name === 'mining_class');
+    const SetGreeting = CONTRACT_ABI.find(i => i.name === 'nft_id');
     const interface = new ethers.utils.Interface([SetGreeting]);
     const encodedFunction = interface.encodeFunctionData(`${SetGreeting.name}`, [greeting]);
     console.log({ encodedFunction });
